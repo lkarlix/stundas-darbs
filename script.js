fe7dfc728b18c5cdd37e9d0900a12c250eb092d0
lkarlix <karlis.ezerkalns@gmail.com>
2025-02-14 10:25:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+/*
+alert("Hello World");
+
+console.log("Hello World!");
+*/
+/*
+let x = 5;
+let y = 12;
+let result = y % x;
+
+if(x == y)
+{
+    console.log("x ir vienāds ar y");
+}
+*/
+
+
+let number = prompt("Enter number: ");
+if(number > 0)
+{
+    console.log(number , "ir pozitīvs");
+}
+if(number < 0)
+{
+    console.log(number , "ir negatīvs");
+}
+if(number == 0)
+{
+    console.log(number , "ir 0");
+}
